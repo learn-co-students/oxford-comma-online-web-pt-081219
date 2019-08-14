a1cb0f3074424e6f4201d7d9ee42c33d232ed144
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-14 01:15:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   else
-    return array[0..-2].join(', ') + ", and " + array[-1]
+    return array[0..-2].join(", ") +(", and ") + array[-1]
   end
 end
- oxford_comma(["1","2","3","4","5"])
+ puts oxford_comma(["1","2","3","4","5"])
