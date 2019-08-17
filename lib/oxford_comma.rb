5c195ed5ff218df6828b7c50f879de92da76893d
Daniel Reina <dannyflatiron@gmail.com>
2019-08-17 16:30:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#   string = ""
+#   if array.length < 2
+#     string = array.join("")
+#   end
+#   string
+# end
 
+
+def oxford_comma(array)
+  if array.length < 2
+    p array.join("")
+  elsif array.length < 3
+  p array.join(" and ")
+elsif array.length < 4
+  array[-1].insert(0, "and ")
+  p  array.join(", ")
+else array.length < 5
+  array[-1].insert(0, "and ")
+  p  array.join(", ")
+  end
 end
