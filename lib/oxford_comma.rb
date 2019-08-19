b5c41bab13caf8d00716f530dad01c618a91d55e
Christian Cain <christian22cain@yahoo.com>
2019-08-19 15:50:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  new_array = []
+  new_string = []
+  
+  if array.length == 1
+    new_string = array[0]
+  elsif array.length == 2
+    new_string = array.join(" and ")
+  else
+    array.each do |element|
+      if element == array.last
+        new_array.push("and #{element}")
+      elsif
+        new_array.push("#{element}, ")
+      end
+    end
+    
+    new_string = new_array.join
+  end
+  
+  return new_string
 end
