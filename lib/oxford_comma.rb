8a2052b1ecaf6ebd00f17d0f05ee624481a787d9
Ashley Norton <nortonashleym@gmail.com>
2019-08-22 18:32:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 2
+   array.join(" and ")
+  elsif array.size >= 3
+    last_element = array.pop
+    new_string = array.insert(-1, "and ").join(", ")
+    new_string << last_element
+  else 
+    array.join 
+  end
 end
