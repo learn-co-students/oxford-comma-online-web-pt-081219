3ac290e6744a963e33679c93ec64d6953350ce78
Baktiar Miah <baktiar.m@gmail.com>
2019-08-23 19:41:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  if arr.size==1
+    return arr[0]
+  else if arr.size == 2
+    return arr[0] + ' and ' + arr[1] 
+    
+  else
+    lword = arr[arr.size-1] 
+    lword = 'and '+lword
+    arr[arr.size-1] = lword
+    return arr.join(', ')
+    end
+  end
 end
