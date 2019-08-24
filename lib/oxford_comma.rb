b84c1ad6cdae115d0c1da698d3517eba738daa99
Sheriefs Gamble <sgamble284@gmail.com>
2019-08-24 10:40:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    array[-1] = "and #{array[-1]}"
+    mystring = array.join(", ")
+    mystring
+  end
 end
